47f893ebca495816c0f53620078faeb00eaaa4c4
Byron Motoche <bmotoche@stackbuilders.com>
2022-01-06 16:59:35 -0500

    add dockerfiles and update messages responses

--- api/messages/message.py
+++ api/messages/message.py
@@ -1,3 +1,7 @@
 class Message:
     def __init__(self, text):
         self.text = text
+        self.metadata = {
+            "api": "api_flask_python_hello-world",
+            "branch": "basic-role-based-access-control"
+        }
--- api/messages/messages_views.py
+++ api/messages/messages_views.py
@@ -21,6 +21,7 @@ bp = Blueprint(bp_name, __name__, url_prefix=bp_url_prefix)
 @bp.route("/public")
 def public():
     return {
+        "metadata": get_public_message().metadata,
         "text": get_public_message().text
     }
 
@@ -29,6 +30,7 @@ def public():
 @authorization_guard
 def protected():
     return {
+        "metadata": get_protected_message().metadata,
         "text": get_protected_message().text
     }
 
@@ -38,5 +40,6 @@ def protected():
 @permissions_guard([admin_messages_permissions.read])
 def admin():
     return {
+        "metadata": get_admin_message().metadata,
         "text": get_admin_message().text
     }
